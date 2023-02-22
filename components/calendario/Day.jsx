597904abbbfde3918c4770837823d1452145c1c6
daniel <danielnava901@hotmail.com>
2023-02-22 10:22:50 -0600

    commit

--- components/calendario/Day.jsx
+++ components/calendario/Day.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Day = ({day, month, isToday}) => {
+    let [_isToday, _setIsToday] = useState(0);
+
+    useEffect(() => {
+        _setIsToday(isToday);
+    }, [isToday]);
+
     return <div className={`w-2/12 h-14 md:h-28 p-2 flex flex-col text-xs md:text-lg
     border flex flex-col ${isToday ? "bg-gray-200" : ''}`}
     style={{
